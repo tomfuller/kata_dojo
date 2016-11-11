24cf9f810dda49abae541e9b14555810797b2d7e
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-11 09:39:57 +0000

    finished again

--- fizzbuzz_kata/spec/fizzbuzz_spec.rb
+++ fizzbuzz_kata/spec/fizzbuzz_spec.rb
@@ -1,24 +1,24 @@
 require 'fizzbuzz'
 
-describe "fizzbuzz" do
+describe 'fizzbuzz' do
 
   context 'divisibility tests' do
 
-    it 'should test for divisibility by 3' do
-      expect(divisible_by_3?(3)).to eq true
-    end
+     it 'should test for divisibility by 3' do
+       expect(divisible_by_3?(3)).to eq true
+     end
 
-    it 'should test for divisibility by 5' do
-      expect(divisible_by_5?(5)).to eq true
-    end
+     it 'should test for divisibility by 5' do
+       expect(divisible_by_5?(5)).to eq true
+     end
 
-    it 'should test for divisibility by 15' do
-      expect(divisible_by_15?(15)).to eq true
-    end
+     it 'should test for divisibility by 15' do
+       expect(divisible_by_15?(15)).to eq true
+     end
 
-    it 'should test for divisibility of number by divisor' do
-      expect(divisible_by?(3, 3)).to eq true
-    end
+     it 'should test for disibility or number by divisor' do
+       expect(divisible_by?(3, 3)).to eq true
+     end
 
   end
 
@@ -29,14 +29,14 @@ describe "fizzbuzz" do
     end
 
     it 'should test for indivisibility by 5' do
-      expect(divisible_by_5?(7)).to eq false
+      expect(divisible_by_5?(6)).to eq false
     end
 
     it 'should test for indivisibility by 15' do
       expect(divisible_by_15?(16)).to eq false
     end
 
-    it 'should test for indivisibilty of number by divisor' do
+    it 'should test for indivisibility of number by divisor' do
       expect(divisible_by?(4, 3)).to eq false
     end
 
@@ -44,21 +44,22 @@ describe "fizzbuzz" do
 
   context 'fizzbuzz tests' do
 
-    it 'should return "fizz" when divisible by 3' do
+    it 'should return "fizz" when passed multiple of 3' do
       expect(fizzbuzz(3)).to eq 'fizz'
     end
 
-    it 'should return "buzz" when divisible by 5' do
+    it 'should return "buzz" when passed multiple of 5' do
       expect(fizzbuzz(5)).to eq 'buzz'
     end
 
-    it 'should return "fizzbuzz" when divisible by 15' do
+    it 'should return "fizzbuzz" when passed multiple of 3 and 5' do
       expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
 
-    it 'should return number if indivisible by 3, 5 or both' do
+    it 'should return number when not a multiple of 3, 5 or both' do
       expect(fizzbuzz(2)).to eq 2
     end
+
 
   end
 
